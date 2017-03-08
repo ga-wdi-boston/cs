2316eb594b177b28aac9eeb79a4ab42897b3ead8
Antony Donovan <antony.donovan@generalassemb.ly>
2017-03-08 17:17:50 -0500

    Add initial solution
    One commit on 016/master

--- lib/queue.rb
+++ lib/queue.rb
@@ -2,4 +2,20 @@
 
 # A queue class
 class Fifo
+  def initialize
+    @store = []
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+  def enqueue(value)
+    @store.push(value)
+    self
+  end
+
+  def dequeue
+    @store.shift
+  end
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -2,4 +2,20 @@
 
 # A queue class
 class Stack
+  def initialize
+    @store = []
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+  def push(value)
+    @store.push(value)
+    self
+  end
+
+  def pop
+    @store.pop
+  end
 end
