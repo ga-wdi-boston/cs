77104ab476988d19310c9db37d5ac52d44eb181f
Antony Donovan <antony.donovan@generalassemb.ly>
2017-04-03 12:43:03 -0400

    Solution live coded for cohort 016
    Use to update solution as appropriate

--- lib/queue.rb
+++ lib/queue.rb
@@ -2,4 +2,25 @@
 
 # A queue class
 class Fifo
+  # allocating space for items on our queue
+  def initialize
+    @store = []
+  end
+
+  # Is this queue empty
+  def empty?
+    @store.empty?
+  end
+
+  # add item to the end of the queue
+  # return self to allow method chaining
+  def enqueue(value)
+    @store.push(value)
+    self
+  end
+
+  # remove item from the front of the queue
+  def dequeue
+    @store.shift
+  end
 end
